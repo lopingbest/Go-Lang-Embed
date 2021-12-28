d2aeead0307cd24a37c46f5bca4b484db1814923
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2021-12-28 14:56:59 +0700

    add embed_test, go.mod, version.txt

---
+++ embed_test.go
@@ -0,0 +1,14 @@
+package Go_Lang_Embed
+
+import (
+	_ "embed"
+	"fmt"
+	"testing"
+)
+
+//go:embed version.txt
+var version string
+
+func TestString(t *testing.T) {
+	fmt.Println(version)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/golang_embed
+
+go 1.17
