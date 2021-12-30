28c615d32168460b229691023db3269125a325a7
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2021-12-30 13:31:30 +0700

    feat:Go-Lang-Embed add TestByte, embed photo to byte, and file png

--- embed_test.go
+++ embed_test.go
@@ -3,6 +3,8 @@ package Go_Lang_Embed
 import (
 	_ "embed"
 	"fmt"
+	"io/fs"
+	"io/ioutil"
 	"testing"
 )
 
@@ -11,4 +13,15 @@ var version string
 
 func TestString(t *testing.T) {
 	fmt.Println(version)
+}
+
+//go:embed google.png
+var logo []byte
+
+func TestByte(t *testing.T) {
+	//save menjadi file baru dengan data yang sama
+	err := ioutil.WriteFile("logo_new.png", logo, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 }
